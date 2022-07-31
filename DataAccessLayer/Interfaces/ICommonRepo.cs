96668697c8fc04e7a0e52c040397e095e23ce7e0
luckyman1004 <luckysuperdev@gmail.com>
2022-08-01 04:50:33 +0600

    fix: db

---
+++ DataAccessLayer/Interfaces/ICommonRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Interfaces
+{
+    public interface ICommonRepo<Class, ID>
+    {
+        List<Class> Get();
+        Class Get(ID id);
+        bool Create(Class obj);
+        bool Update(Class obj);
+        bool Delete(ID id);
+    }
+}
---
+++ DataAccessLayer/Repo/PackageRepo.cs
@@ -0,0 +1,48 @@
+﻿using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class PackageRepo
+    {
+        static WebSeriesDBEntities db;
+
+        static PackageRepo()
+        {
+            db = new WebSeriesDBEntities();
+        }
+
+        public static List<Package> Get()
+        {
+            return db.Packages.ToList();
+        }
+
+        public static Package Get(int id)
+        {
+            return db.Packages.FirstOrDefault(p => p.Id == id);
+        }
+
+        public static void Create(Package p)
+        {
+            db.Packages.Add(p);
+            db.SaveChanges();
+        }
+
+        public static void Update(Package p)
+        {
+            var edit = db.Packages.FirstOrDefault(package => package.Id == p.Id);
+            db.Entry(edit).CurrentValues.SetValues(p);
+            db.SaveChanges();
+        }
+
+        public static void Delete(int id)
+        {
+            var del = db.Packages.FirstOrDefault(d => d.Id == id);
+            db.Packages.Remove(del);
+        }
+    }
+}
