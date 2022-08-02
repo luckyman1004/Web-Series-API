2045db26a56f7427478df37a7eb2a6ce96e106bb
luckyman1004 <luckysuperdev@gmail.com>
2022-08-02 23:06:42 +0600

    Comment deleted after test

--- Web-Series-API--ASP.NET/Controllers/PackageController.cs
+++ Web-Series-API--ASP.NET/Controllers/PackageController.cs
@@ -23,8 +23,6 @@ namespace Web_Series_API__ASP.NET.Controllers
         public HttpResponseMessage GetAll()
         {
             return Request.CreateResponse(HttpStatusCode.OK, PackageService.Get());
-
-            //new
         }
 
         [Route("api/package/create")]
